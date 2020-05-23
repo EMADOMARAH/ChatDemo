83c34c621cb362673405f3b4b2545d82d73ef57a
EMADOMARAH <31766091+EMADOMARAH@users.noreply.github.com>
2020-05-23 21:21:10 +0200

    Initial commit

---
+++ app/src/main/java/softagi/chatad/Fragments/ChatsFragment.java
@@ -0,0 +1,25 @@
+package softagi.chatad.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import softagi.chatad.R;
+
+public class ChatsFragment extends Fragment
+{
+    View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        view = inflater.inflate(R.layout.chats_fragment, container, false);
+        return view;
+    }
+}
